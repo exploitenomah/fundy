608dc27cdb63123ec85934ca2fb99410547c363c
exploitenomah <iksploiting@gmail.com>
2023-01-03 12:39:31 +0100

    rename ethereum to eth

---
+++ eth/contracts/Campaign.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint256 value;
+        address payable recipient;
+        bool complete;
+        uint256 approvalCount;
+        mapping(address => bool) approvers;
+    }
+    address public manager;
+    uint256 public minimumContribution;
+    mapping(address => bool) public contributors;
+    uint256 public contributorsCount;
+    Request[] public requests;
+
+    modifier restrictToManager() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    constructor(uint256 minimum, address creator) {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        contributors[msg.sender] = true;
+        contributorsCount++;
+    }
+
+    function createRequest(
+        string memory description,
+        uint256 value,
+        address payable recipient
+    ) public restrictToManager {
+        Request storage newRequest = requests.push();
+        newRequest.description = description;
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
+    }
+
+    function approveRequest(uint256 index) public {
+        Request storage request = requests[index];
+
+        require(contributors[msg.sender]);
+        require(!request.approvers[msg.sender]);
+
+        request.approvers[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint256 index) public restrictToManager {
+        Request storage request = requests[index];
+
+        require(!request.complete);
+        require(request.approvalCount > (contributorsCount / 2));
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
---
+++ eth/deploy.js
@@ -0,0 +1,5 @@
+const CampaignFactory = require('./contractBuilds/CampaignFactory')
+const Campaign = require('./contractBuilds/Campaign.json')
+
+
+const x= 'ysdf'
