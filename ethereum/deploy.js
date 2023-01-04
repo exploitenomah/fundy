2568952c5dbfede64d07686eabed250f5054f09b
exploitenomah <iksploiting@gmail.com>
2023-01-04 21:09:59 +0100

    create contract deploy script

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,5 +1,27 @@
-const CampaignFactory = require('./contractBuilds/CampaignFactory')
-const Campaign = require('./contractBuilds/Campaign.json')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const { interface, bytecode } = require('./contractBuilds/CampaignFactory.json')
+const dotenv = require('dotenv')
+const { storeDeployedContractInfo } = require('./mongodb')
 
+dotenv.config('./.env')
 
-const x= 'ysdf'
+const provider = new HDWalletProvider(
+   process.env.SECRET_PHRASE,
+   process.env.INFURA_URL,
+)
+
+const web3 = new Web3(provider)
+
+const deploy = async () => {
+  let accounts = await web3.eth.getAccounts()
+
+  console.log('attempting to deploy from account: ==>', + ' ' + accounts[0])
+
+  const deployment = await new web3.eth.Contract( interface )
+  .deploy({data: bytecode, arguments: []})
+  .send({ gas: 3000000, from: accounts[0]})
+  const storedContractData = await storeDeployedContractInfo({ abi: interface, address: deployment.options.address})
+}
+
+deploy()
