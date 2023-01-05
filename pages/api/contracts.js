77ceae493d92e149c9bbd7b9f4533f4b1f04337e
exploitenomah <iksploiting@gmail.com>
2023-01-05 22:08:30 +0100

    create route for uploading most recent contract deploys to db

--- pages/api/contracts.js
+++ pages/api/contracts.js
@@ -20,4 +20,30 @@ export default async function handler(req, res) {
       await client.close()
     }
   }
+
+  if(req.method === 'POST'){
+    const {client, db} = await connect('campaign_factory')
+    try{
+      await db.collection('campaign_factory').findOneAndUpdate(
+        { is_most_recent: true}, 
+        { $set: {is_most_recent: false, }},
+        { upsert: false })
+      const currentLatest = await db.collection('campaign_factory').insertOne({
+        abi: req.body.abi, 
+        address: req.body.address, 
+        deployed_at: new Date(),
+        created_at: new Date(),
+        is_most_recent: true
+      })
+      return res.status(200).json({ contract: currentLatest})
+    }catch(err){
+      console.error('UNABLE TO SAVE CONTRACT!!! ❌❌❌❌❌ ERROR: ==>>> ', err)
+      return res.status(500).json({
+        message: 'unable to get contract!',
+        contract: null
+      })
+    }finally{
+      await client.close()
+    }
+  }
 }
