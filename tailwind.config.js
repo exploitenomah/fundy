42c14648b68b9eb1eb7b39124a89ee3bb39f4d18
exploitenomah <iksploiting@gmail.com>
2023-01-06 21:57:29 +0100

    create basic animmations for loaders

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,37 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'defaultLoader': {
+          '0%': {
+            transform: 'translate(var(--tw-translate-x), var(--tw-translate-y)) rotateX(0deg) rotateY(0deg) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))',
+          },
+          '50%': {
+            transform: 'translate(var(--tw-translate-x), var(--tw-translate-y)) rotateX(0deg) rotateY(180deg) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))',
+          }, 
+          '100%': {
+            transform: 'translate(var(--tw-translate-x), var(--tw-translate-y)) rotateX(180deg) rotateY(180deg) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))',
+          }
+        },
+        'widthChange': {
+          '0%': {
+            width: 0
+          },
+          '50%': {
+            width: '100%',
+          },
+          '100%': {
+            width: 0,
+          }
+        }
+      },
+      animation: {
+        'defaultLoader': 'defaultLoader 1.5s ease-in-out infinite',
+        'widthChange': 'widthChange 5s linear infinite'
+      }
+    },
   },
-  plugins: [],
+  plugins: []
 }
+/** @type {import('tailwindcss').Config} */
