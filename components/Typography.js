9e77e2e921e5eaa607091e37d790ef0cdd6a5bbf
exploitenomah <iksploiting@gmail.com>
2023-01-10 15:17:31 +0100

    create typography components

---
+++ components/Typography.js
@@ -0,0 +1,21 @@
+import { createElement } from "react"
+
+export default function TypographyBase({ children, className, as, ...props}) {
+
+  const typographyClassName = `prose prose-lg lg:prose-xl leading-normal tracking-wide drop-shadow-xl ${className}`
+  if(as){
+    return (
+      createElement(
+        as,
+        {...props, className: typographyClassName},
+        children
+      )
+    )
+  }
+
+  return (
+    <p className={typographyClassName}>
+      {children}
+    </p>
+  )
+}
