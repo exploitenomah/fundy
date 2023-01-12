17a2937120f81c5f56f3774f654e724722424a8d
exploitenomah <iksploiting@gmail.com>
2023-01-12 15:55:38 +0100

    fix linting errors

--- components/Loader.js
+++ components/Loader.js
@@ -2,16 +2,16 @@
 
 export default function DefaultLoader({ children }) {
 
-  return (
-    <div 
-      aria-busy={true} 
-      role="progressbar" 
-      aria-valuetext="Loading Application ndetermined" 
-      aria-live="assertive"
-      className='h-screen w-screen flex items-center justify-center flex-col'
-      style={{ perspective: '200px'}} >
-      <div className="w-[100px] h-[100px] bg-white rounded-xl animate-defaultLoader my-3"></div>
-      {children}
-    </div>
-  )
+	return (
+		<div 
+			aria-busy={true} 
+			role="progressbar" 
+			aria-valuetext="Loading Application ndetermined" 
+			aria-live="assertive"
+			className='h-screen w-screen flex items-center justify-center flex-col'
+			style={{ perspective: '200px'}} >
+			<div className="w-[100px] h-[100px] bg-white rounded-xl animate-defaultLoader my-3"></div>
+			{children}
+		</div>
+	)
 }
