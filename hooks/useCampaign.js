bdc3847df2bef65de8dd66e1aaf60cfd311b5607
exploitenomah <iksploiting@gmail.com>
2023-01-12 15:59:45 +0100

    fix linting errors

--- hooks/useCampaign.js
+++ hooks/useCampaign.js
@@ -1,41 +1,41 @@
 
-import useWeb3 from "./useWeb3";
-import { useEffect, useState, useMemo, useCallback } from "react";
+import useWeb3 from './useWeb3'
+import { useEffect, useState, useMemo, useCallback } from 'react'
 
 export const useCampaignFactory = () => {
-  const [contractData, setContractData] = useState(null)
+	const [contractData, setContractData] = useState(null)
 
-  const web3 = useWeb3()
-  const campaignFactoryInterface = useMemo(() => {
-    if(contractData !== null){
-      return new web3.eth.Contract(
-        contractData.abi,
-        contractData.address
-      )
-    }else return null
-  }, [contractData, web3.eth.Contract])
+	const web3 = useWeb3()
+	const campaignFactoryInterface = useMemo(() => {
+		if(contractData !== null){
+			return new web3.eth.Contract(
+				contractData.abi,
+				contractData.address
+			)
+		}else return null
+	}, [contractData, web3.eth.Contract])
 
-  const getContractData = useCallback(async () => {
-   try{
-    const res = await fetch('/api/contracts')
-    if(res.status === 200){
-      const { contract } = await res.json()
-      setContractData(contract)
-    }
-   }catch(err){
-    console.error(err)
-   }
-  }, [])
+	const getContractData = useCallback(async () => {
+		try{
+			const res = await fetch('/api/contracts')
+			if(res.status === 200){
+				const { contract } = await res.json()
+				setContractData(contract)
+			}
+		}catch(err){
+			console.error(err)
+		}
+	}, [])
   
-  useEffect(() => {
-    if(contractData === null){
-      try{
-        getContractData()
-      }catch(err){
-        console.error(error)
-      }
-    }
-  }, [contractData, getContractData])
+	useEffect(() => {
+		if(contractData === null){
+			try{
+				getContractData()
+			}catch(err){
+				console.error(err)
+			}
+		}
+	}, [contractData, getContractData])
 
-  return campaignFactoryInterface
+	return campaignFactoryInterface
 }
