5ca883f275dd50f0a4dd90fbd9e2fda4e7b7f69f
exploitenomah <iksploiting@gmail.com>
2023-01-14 20:51:53 +0100

    fix linting errors

--- pages/_app.js
+++ pages/_app.js
@@ -6,60 +6,60 @@ import Layout from '../components/Layout'
 import Notification from '../components/Notificaton'
 
 export default function App({ Component, pageProps }) {
-  const { campaignFactory, web3 } = useCampaignFactory()
+	const { campaignFactory, web3 } = useCampaignFactory()
 
-  const [store, setStore] = useState({
-  	campaigns: [],
-    message: '',
-    showMsg: false,
-    msgStatus: 'info'
-  })
+	const [store, setStore] = useState({
+		campaigns: [],
+		message: '',
+		showMsg: false,
+		msgStatus: 'info'
+	})
 
-  const getCampaigns = useCallback(async () => {
-  	if (campaignFactory) {
-  		const campaigns = await campaignFactory.methods.getDeployedCampaigns().call()
-  		setStore(prev => ({
-  			...prev, campaigns
-  		}))
-  	}
-  }, [campaignFactory])
+	const getCampaigns = useCallback(async () => {
+		if (campaignFactory) {
+			const campaigns = await campaignFactory.methods.getDeployedCampaigns().call()
+			setStore(prev => ({
+				...prev, campaigns
+			}))
+		}
+	}, [campaignFactory])
 
-  useEffect(() => {
-  	getCampaigns()
-    const removeMsg = store.showMsg ? setTimeout(() => {
-      setStore(prev => ({
-        ...prev, msgStatus: 'info', message: '', showMsg: false
-      }))
-    }, 10000) : undefined
+	useEffect(() => {
+		getCampaigns()
+		const removeMsg = store.showMsg ? setTimeout(() => {
+			setStore(prev => ({
+				...prev, msgStatus: 'info', message: '', showMsg: false
+			}))
+		}, 10000) : undefined
 
-    return () => {
-      clearTimeout(removeMsg)
-    }
-  }, [getCampaigns, store.showMsg])
+		return () => {
+			clearTimeout(removeMsg)
+		}
+	}, [getCampaigns, store.showMsg])
 
-  if ('campaignFactory' === null) {
-    return (
-      <div className='h-screen w-screen flex flex-col justify-center items-center'>
-        <DefaultLoader>
-          <TextLoader text={'Loading Fundy...'} />
-        </DefaultLoader>
-      </div>
-    )
-  } else {
-    return (
-      <Layout title={Component.title}>
-          <Notification 
-           hide={() => setStore(prev => ({...prev, showMsg: false}))}
-           message={store.message} 
-           show={store.showMsg} 
-           status={store.msgStatus} />
-          <Component
-            web3={web3}
-            store={store}
-            setStore={setStore}
-            factory={campaignFactory} 
-            {...pageProps} />
-      </Layout>
-    )
-  }
+	if (campaignFactory === null) {
+		return (
+			<div className='h-screen w-screen flex flex-col justify-center items-center'>
+				<DefaultLoader>
+					<TextLoader text={'Loading Fundy...'} />
+				</DefaultLoader>
+			</div>
+		)
+	} else {
+		return (
+			<Layout title={Component.title}>
+				<Notification 
+					hide={() => setStore(prev => ({...prev, showMsg: false}))}
+					message={store.message} 
+					show={store.showMsg} 
+					status={store.msgStatus} />
+				<Component
+					web3={web3}
+					store={store}
+					setStore={setStore}
+					factory={campaignFactory} 
+					{...pageProps} />
+			</Layout>
+		)
+	}
 }
