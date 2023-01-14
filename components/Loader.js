e18c2b9c4a03c1dec2e80f2e3b63c6448c33dc83
exploitenomah <iksploiting@gmail.com>
2023-01-14 20:50:53 +0100

    fix linting errors

--- components/Loader.js
+++ components/Loader.js
@@ -18,13 +18,13 @@ export default function DefaultLoader({ children }) {
 
 export const TextLoader = ({ text, className }) => {
 
-  return (
-    <div data-text={text}
-    className={`text-white relative font-black text-small uppercase \n
+	return (
+		<div data-text={text}
+			className={`text-white relative font-black text-small uppercase \n
      before:content-[attr(data-text)] before:absolute before:text-purple-400 \n
      before:z-0 before:animate-widthChange before:overflow-hidden \n
      before:whitespace-nowrap before:w-full before:border-r before:border-r-current ${className}`}>
-     {text}
-  </div>
-  )
+			{text}
+		</div>
+	)
 }
