300ebf0bb3ad5f08eaa3d549b18dd4c024763c34
exploitenomah <iksploiting@gmail.com>
2023-01-15 09:05:23 +0100

    fix linting errors

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -8,35 +8,35 @@ const axios = require('axios')
 dotenv.config('../.env')
 
 const provider = new HDWalletProvider(
-   process.env.SECRET_PHRASE,
-   process.env.INFURA_URL,
+	process.env.SECRET_PHRASE,
+	process.env.INFURA_URL,
 )
 
 const web3 = new Web3(provider)
 
-const deploy = async () => {
-  let accounts = await web3.eth.getAccounts()
+const deploy = async () => {	
+	let accounts = await web3.eth.getAccounts()
 
-  console.log('attempting to deploy from account: ==>', + ' ' + accounts[0])
+	console.log('attempting to deploy from account: ==>', + ' ' + accounts[0])
 
-  try{
-    const deployment = await new web3.eth.Contract( contractInterface )
-    .deploy({data: bytecode, arguments: []})
-    .send({ gas: 3000000, from: accounts[0]})
+	try{
+		const deployment = await new web3.eth.Contract( contractInterface )
+			.deploy({data: bytecode, arguments: []})
+			.send({ gas: 3000000, from: accounts[0]})
 
-    const done = await axios({
-      method: 'POST',
-      url: 'http:127.0.0.1:3000/api/contracts', 
-      data: {
-        abi: contractInterface, address: deployment.options.address
-      }
-    })
-    console.log(done.data, 'done.')
-    process.exit()
-  }catch(err){
-    console.log(err)
-    process.exit()
-  }
+		const done = await axios({
+			method: 'POST',
+			url: 'http:127.0.0.1:3000/api/contracts', 
+			data: {
+				abi: contractInterface, address: deployment.options.address
+			}
+		})
+		console.log(done.data, 'done.')
+		process.exit()
+	}catch(err){
+		console.log(err)
+		process.exit()
+	}
 }
 
 deploy()
