aa37591ad2a58411db803f00879c001db463bbcf
exploitenomah <iksploiting@gmail.com>
2023-01-15 22:15:24 +0100

    create single campaign route

--- pages/campaigns/[address]/index.js
+++ pages/campaigns/[address]/index.js
@@ -5,10 +5,11 @@ import { useCallback, useEffect, useMemo, useState } from 'react'
 import useNewContract from '../../../hooks/useNewContract'
 import TypographyBase from '../../../components/Typography'
 import { H2, H3 } from '../../../components/Headings'
+import ContributionButton from '../../../components/ContributionButton'
 
 const InfoCard = ({ title, details, info, children }) => {
 	return (
-		<article className='flex flex-col border bg-white/10 px-6 py-4 max-w-sm'>
+		<article className='flex flex-col border bg-white/10 px-6 py-4 mx-auto w-[80vw] max-w-sm'>
 			<H3 className='inline-block text-purple-100 break-words'>{title}</H3>
 			<TypographyBase
 				className=' text-white/90 my-3'>{details}</TypographyBase>
@@ -64,41 +65,45 @@ export default function Campaign({ web3 }) {
 
 	return (
 		<>
-			<main className='my-8 max-w-[85vw] mx-auto'>
-				<H2 as='h1' className='text-md'>
-					Campaign deployed at <span className='text-white/60 max-w-[800px] overflow-hidden text-ellipsis text-md block'>{address}</span>
-				</H2>
-				<div className='my-8 flex justify-start flex-wrap gap-y-8 gap-x-8'>
-
-					<InfoCard 
-						details={'Manager'}
-						title={contractSummary.manager}
-						info={'The manager is the creator of this contract and can create requests to withdraw funds.'}>
-						{userAccount === contractSummary.manager ?
-							<small className='text-purple-100'>
-								You are the manager of this campaign.
-							</small> : null}
-					</InfoCard>
-					<InfoCard
-						details={'About This Campaign'}
-						title={contractSummary.about}
-						info={''} />
-					<InfoCard
-						details={'Campaign Balance'}
-						title={`${contractSummary.balance} ether`}
-						info={'The currrent financial balance of this campaign.'} />
-					<InfoCard
-						details={'Minimum Contribution'}
-						title={`${contractSummary.minimumContribution} wei`}
-						info={'This is the minimum amount that can be contributed to this campaign.'} />
-					<InfoCard
-						details={'Current amount of contributors'}
-						title={contractSummary.totalContributors}
-						info={'The number of people who have contributed to this campaign.'} />		
-					<InfoCard
-						details={'Total Count Of Spending Requests'}
-						title={contractSummary.requestsLength}
-						info={'Spending requests are requests made by the manager of the contract to withdraw funds.'} />
+			<main className='mt-32 max-w-[85vw] mx-auto'>
+				<div>
+					<div className='my-8 flex items-center justify-start flex-wrap gap-y-8 gap-x-32 lg:justify-between'>
+						<H2 as='h1' className='text-md'>
+							Campaign deployed at <span className='text-white/60 max-w-[400px] md:text-[1.6rem] overflow-hidden text-ellipsis text-md block'>{address}</span>
+						</H2>
+						<ContributionButton />
+					</div>
+					<div className='my-8 flex justify-start flex-wrap gap-y-8 gap-x-8'>
+						<InfoCard
+							details={'Manager'}
+							title={contractSummary.manager}
+							info={'The manager is the creator of this contract and can create requests to withdraw funds.'}>
+							{userAccount === contractSummary.manager ?
+								<small className='text-purple-100'>
+									You are the manager of this campaign.
+								</small> : null}
+						</InfoCard>
+						<InfoCard
+							details={'About This Campaign'}
+							title={contractSummary.about}
+							info={''} />
+						<InfoCard
+							details={'Campaign Balance'}
+							title={`${contractSummary.balance} ether`}
+							info={'The currrent financial balance of this campaign.'} />
+						<InfoCard
+							details={'Minimum Contribution'}
+							title={`${contractSummary.minimumContribution} wei`}
+							info={'This is the minimum amount that can be contributed to this campaign.'} />
+						<InfoCard
+							details={'Current amount of contributors'}
+							title={contractSummary.totalContributors}
+							info={'The number of people who have contributed to this campaign.'} />
+						<InfoCard
+							details={'Total Count Of Spending Requests'}
+							title={contractSummary.requestsLength}
+							info={'Spending requests are requests made by the manager of the contract to withdraw funds.'} />
+					</div>
 				</div>
 			</main>
 		</>
