de9635fd78bd4810f1d64c20cebb5ba3e8281985
exploitenomah <iksploiting@gmail.com>
2023-01-15 09:06:01 +0100

    handle display when no campaigns are available

--- pages/campaigns/index.js
+++ pages/campaigns/index.js
@@ -1,6 +1,6 @@
 
 import Link from 'next/link'
-import { SecondaryBtn } from '../../components/Buttons'
+import { SecondaryBtn, PrimaryBtn } from '../../components/Buttons'
 import { H1 } from '../../components/Headings'
 import TypographyBase from '../../components/Typography'
 
@@ -13,6 +13,15 @@ export default function Campaigns({ store }) {
 				<TypographyBase className='text-white/80'>
 					Discover campaigns to fund...
 				</TypographyBase>
+				{store.campaigns.length === 0 && 
+				<div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-center'>
+					<div>
+						No Campaigns to show
+					</div>
+					<Link className='my-8 block' href='/campaigns/new'>
+						<PrimaryBtn as='span'>Create Mine</PrimaryBtn>
+					</Link>
+				</div>}
 				<ul className='my-8 flex justify-start items-start gap-x-8 gap-y-6 flex-wrap'>
 					{store.campaigns.map(campaign => (
 						<li 
@@ -22,7 +31,7 @@ export default function Campaigns({ store }) {
 							<span 
 								title={campaign}
 								className='inline-block p-0 m-0 w-[250px] overflow-hidden text-ellipsis'>{campaign}</span>
-							<Link className='my-8' passHref href={`/campaigns/${campaign}`}>
+							<Link className='my-8 w-max-content' passHref href={`/campaigns/${campaign}`}>
 								<SecondaryBtn className='text-purple-300 px-2 py-2 border border-current hover:border-transparent' as='span'>View Campaign</SecondaryBtn>
 							</Link>
 						</li>
