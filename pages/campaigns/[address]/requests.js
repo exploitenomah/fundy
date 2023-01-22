0c4120308723fb5192eba37f1ae1827cb9db7e14
exploitenomah <iksploiting@gmail.com>
2023-01-22 17:39:09 +0100

    add back button to requests page

--- pages/campaigns/[address]/requests.js
+++ pages/campaigns/[address]/requests.js
@@ -7,6 +7,8 @@ import useNewContract from '../../../hooks/useNewContract'
 import { H2, H3, H4, H5 } from '../../../components/Headings'
 import DefaultLoader, { TextLoader } from '../../../components/Loader'
 import { ButtonBase } from '../../../components/Buttons'
+import Link from 'next/link'
+import { FaArrowLeft } from 'react-icons/fa'
 // import ContributionForm from '../../../components/ContributeModal'
 // import { PrimaryBtn } from '../../../components/Buttons'
 
@@ -112,6 +114,9 @@ export default function Requests({ web3, setStore, store }) {
 	return (
 		<>
 			<main className='my-12 max-w-[85vw] mx-auto animate-fade-in'>
+				<Link href={`/campaigns/${address}`}>
+					<ButtonBase className='flex gap-x-3 items-center my-4'><FaArrowLeft /> Back</ButtonBase>
+				</Link>
 				<div className='my-8 flex items-center justify-start flex-wrap gap-y-8 gap-x-32 lg:justify-between'>
 					<H2 as='h1' className='text-md'>
 						Requests for campaign deployed at <span className='text-white/60 max-w-[400px] md:text-[1.6rem] overflow-hidden text-ellipsis text-md block'>{address}</span>
