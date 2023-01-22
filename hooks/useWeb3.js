b6a1b4d0e7d17491b2a44c7d46c8b84edc562ca0
exploitenomah <iksploiting@gmail.com>
2023-01-22 17:10:08 +0100

    create init func to handle web3 initialization

--- hooks/useWeb3.js
+++ hooks/useWeb3.js
@@ -1,26 +1,33 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import Web3 from 'web3'
 
 
 const useWeb3 = () => {
 	const [ethereum, setEthereum] = useState(null)
+	const [primaryAccount, setPrimaryAccount] = useState('')
 	const web3 = useMemo(() => {
 		if(ethereum) return new Web3(ethereum)
 	}, [ethereum])
 
+	const init = useCallback(async () => {
+		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+		setEthereum(window.ethereum)
+		setPrimaryAccount(accounts[0])
+		window.ethereum.on('accountsChanged', function (accounts) {
+			setPrimaryAccount(accounts[0])
+		})
+	}, [])
+
 	useEffect(() => {
 		if(window && window.ethereum){
-			window.ethereum.request({ 
-				method: 'eth_requestAccounts'
-			}) 
-			setEthereum(window.ethereum)     
+			ethereum === null && init()
 		}
 		if(typeof window === 'undefined' && !ethereum){
 			const provider = new Web3.providers.HttpProvider('http://127.0.0.1:3000')
 			setEthereum(provider)
 		}
-	}, [])
-	return web3
+	}, [ethereum, init])
+	return { web3, primaryAccount }
 }
 
 export default useWeb3
